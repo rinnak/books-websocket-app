20ca7a2c8840c5b8915e513a19305421899f9071
rinnak <212498408+rinnak@users.noreply.github.com>
2026-03-31 17:42:02 +0300

    final result

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://158.160.203.172:8080",
+    baseURL: window.location.hostname === "localhost"
+        ? "http://158.160.203.172:8080"
+        : "/api",
     auth: {
         username: "admin",
         password: "6812363",
